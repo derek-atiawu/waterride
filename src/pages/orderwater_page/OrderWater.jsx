4e2522c52bbe788dda1752445ddfc12d25b8021f
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2023-08-09 12:37:20 +0000

    dev

--- src/pages/orderwater_page/OrderWater.jsx
+++ src/pages/orderwater_page/OrderWater.jsx
@@ -4,18 +4,25 @@ import "./OrderWater.css";
 function OrderWater() {
   return (
     <div>
-      <img
-        className="image"
-        src="https://static-cse.canva.com/_next/static/assets/wave_w2881xh801_a775fc2518c0a6ca438299351b5d48a3faf59e7348ac4a6f5f88b3b68fd5a06a.png"
-        alt="bg-img"
-      />
-      <h3>water type</h3>
-      <div className="cards grid-container">
-        <div className="grid-item">Sachet Water</div>
-        <div className="grid-item"><img src="https://images.unsplash.com/photo-1519455953755-af066f52f1a6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8c2FjaGV0JTIwd2F0ZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60" alt="" /> </div>
-        <div className="grid-item">Dispenser </div>
+      <div class="image-container">
+        <img
+          src="https://static-cse.canva.com/_next/static/assets/wave_w2881xh801_a775fc2518c0a6ca438299351b5d48a3faf59e7348ac4a6f5f88b3b68fd5a06a.png"
+          alt="Background Image"
+          class="background-image"
+        />
+        <div class="text-overlay">
+          <h1>WateRiDe</h1>
+          <p>Additional text or content.</p>
+        </div>
       </div>
-      
+      <div className="container-button">
+        <button class="start-button">Water Type</button>
+      </div>
+      <div class="grid-container">
+        <div class="grid-item">Column 1</div>
+        <div class="grid-item">Column 2</div>
+        <div class="grid-item">Column 3</div>
+      </div>
     </div>
   );
 }
--- src/pages/waterriderhome_page/WaterRide.jsx
+++ src/pages/waterriderhome_page/WaterRide.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import ".//waterride.css";
+import "./waterride.css";
 
 function WaterRide() {
   return (
     <Container className="homeContainer">
       <div className="heading">
+        <div class="image-container">
+          <img
+            src="https://static-cse.canva.com/_next/static/assets/wave_w2881xh801_a775fc2518c0a6ca438299351b5d48a3faf59e7348ac4a6f5f88b3b68fd5a06a.png"
+            alt="Background Image"
+            class="background-image"
+          />
+          <div class="text-overlay">
+            <h1>WateRiDe</h1>
+            <p>Additional text or content.</p>
+          </div>
+        </div>
         <h1 style={{ color: "gold" }}>Water Supply made quick and easy!</h1>
         <h2 style={{ color: "blue" }}>On your finger tip! Sign up today </h2>
       </div>
